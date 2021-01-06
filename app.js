ac3e2fcef1698b273e4ab05b2bd4edacbaba11c5
ycl-hub <823727840@qq.com>
2021-01-06 15:19:58 +0800

    'express解决axios跨域问题'

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 var express = require('express')
 var bodyParser = require('body-parser') //专门翻译post请求       
 var app = express()
+    //设置各版本跨域 防止不兼容  要放在子路由之前  放在app=express()之后 可以解决axios跨域问题
+app.all('*', function(req, res, next) {
+    //设为指定的域
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    // @ts-ignore
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header("X-Powered-By", ' 3.2.1');
+    next();
+});
+process.on('uncaughtException', function(err) {
+    //打印出错误
+    console.log(err);
+    //打印出错误的调用栈方便调试
+    console.log(err.stack);
+});
 var path = require('path')
 
 app.listen(3000, () => {
@@ -22,19 +40,3 @@ app.use('/js', express.static(path.join(__dirname, '/js')))
 app.use('/bootstrap-3.3.7-dist', express.static(path.join(__dirname, '/bootstrap-3.3.7-dist')))
 app.use('/layui', express.static(path.join(__dirname, '/layui')))
 app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')))
-
-//设置各版本跨域 防止不兼容
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With,xtoken");
-    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By", ' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
-});
-process.on('uncaughtException', function(err) {
-    //打印出错误
-    console.log(err);
-    //打印出错误的调用栈方便调试
-    console.log(err.stack);
-});
